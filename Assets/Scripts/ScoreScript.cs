b119e0c19959ff425d2df25bce8b91bcd787b0c8
Alex <alguti@zoho.com>
2023-05-24 18:26:19 +0200

    added credits

--- Assets/Scripts/ScoreScript.cs
+++ Assets/Scripts/ScoreScript.cs
@@ -7,12 +7,15 @@ public class ScoreScript : MonoBehaviour
 {
 
     public TMP_Text text_score;
+    public TMP_Text text_credits;
+
+    private bool _startAnimation = true;
 
     // Start is called before the first frame update
     void Start()
     {
         //Disable/hide the UI
-        CanvasStatsScript.instance.cleanAllStats();
+        /*CanvasStatsScript.instance.cleanAllStats();
         GameManager.instance.disablePlayerIcons();
         GameManager.instance.disableUIBackground();
 
@@ -23,7 +26,34 @@ public class ScoreScript : MonoBehaviour
             GameManager.playerYellowKeys * 3 +
             GameManager.playerGreenKeys * 5 +
             GameManager.playerRedKeys * 6;
-        text_score.SetText(score.ToString());
+        text_score.SetText(score.ToString());*/
+        
     }
-
+    private void Update()
+    {
+        
+    }
+    private void Awake()
+    {
+        if (_startAnimation)
+        {
+            _startAnimation = false;
+            StartCoroutine(moveCredits());
+        }
+    }
+    IEnumerator moveCredits()
+    {
+        float currentTime = 19.0f;
+        //Animation slowly scroll the text
+        float animationTime = 0.1f;
+        yield return new WaitForSeconds(2f);
+        while (currentTime > 0f)
+        {
+            Debug.Log(currentTime);
+            yield return new WaitForSeconds(animationTime);
+            text_credits.transform.position = new Vector3(text_credits.transform.position.x, (text_credits.transform.position.y+0.1f), text_credits.transform.position.z);
+            currentTime -= animationTime;
+        }
+        
+    }
 }
